3fe8195fdcbdc3ef518a3b30a8c9e2f92b852afe
Cygran <apcanning90@gmail.com>
2025-03-08 22:57:18 +0000

    Refactor client main loop to handle game commands and improve user interaction

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
-	"os/signal"
 
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
@@ -30,7 +28,7 @@ func main() {
 		log.Fatalf("Unable to fecth username: %s", err)
 	}
 
-	//Create and bind to queue
+	//Create and bind to pause queue
 	queueName := fmt.Sprintf("%s.%s", routing.PauseKey, userName)
 	_, queue, err := pubsub.DeclareAndBind(connection, routing.ExchangePerilDirect, queueName, routing.PauseKey, pubsub.QueueTypeTransient)
 	if err != nil {
@@ -38,11 +36,37 @@ func main() {
 	}
 	fmt.Printf("Queue %v declared and bound!\n", queue.Name)
 
-	//handle closure from ctrl + c gracefully
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, os.Interrupt)
-	<-signalChan
-
-	fmt.Println("Recieved interrupt. Closing connection")
-	connection.Close()
+	//Game loop
+	gs := gamelogic.NewGameState(userName)
+	for {
+		words := gamelogic.GetInput()
+		if len(words) == 0 {
+			continue
+		}
+		switch words[0] {
+		case "spawn":
+			err := gs.CommandSpawn(words)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+		case "move":
+			_, err = gs.CommandMove(words)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+		case "status":
+			gs.CommandStatus()
+		case "help":
+			gamelogic.PrintClientHelp()
+		case "spam":
+			fmt.Println("Spamming not allowed yet!")
+		case "quit":
+			gamelogic.PrintQuit()
+			return
+		default:
+			fmt.Println("unknown command")
+		}
+	}
 }
